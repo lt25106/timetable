f46b4ed58d63916f9cd971376841d11aeb27387c
name <11pro12ispro@gmail.com>
2025-07-12 16:16:24 +0800

    var become let

--- j.js
+++ j.js
@@ -1,16 +1,16 @@
 const subjectpopup = document.getElementById("subject-popup");
 const timetable = document.getElementById("timetable-grid");
 
-var monday = 22;
-var tuesday = 24;
-var wednesday = 26;
-var thursday = 28;
-var friday = 30;
+let monday = 22;
+let tuesday = 24;
+let wednesday = 26;
+let thursday = 28;
+let friday = 30;
 
-var buttonpressed;
+let buttonpressed;
 
 // in half hrs
-var timeleft = {"Break": 17};
+let timeleft = {"Break": 17};
 ["PC", "CM", "BL", "CS", "DV", "HU", "PE", "AR_MU", "ELECTIVE"].forEach(key => timeleft[key] = 4);
 ["EL", "CCA", "CAM"].forEach(key => timeleft[key] = 8);
 ["MT", "MA"].forEach(key => timeleft[key] = 6);
